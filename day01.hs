89567c8a5fa16fc67c58785ccb3176714d4f2a3b
Henri Lahtinen <splitti@gmail.com>
2024-12-02 21:37:00 +0000

    day01: cleanups

--- day01.hs
+++ day01.hs
@@ -8,7 +8,7 @@ part1 :: [[Int]] -> String
 part1 = ("Part 1: " ++) . show . sum . map abs . (\[left, right] -> zipWith (-) left right)
 
 part2 :: [[Int]] -> String
-part2 = ("Part 2: " ++) . show . sum . (\[left, right] -> map (\n -> n * (length $ filter (== n) right)) left)
+part2 = ("Part 2: " ++) . show . sum . (\[left, right] -> (\n -> n * length (filter (== n) right)) <$> left)
 
 parse :: String -> [[Int]]
 parse = map sort . transpose . map (map read . words) . lines
