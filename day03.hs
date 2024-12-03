4450f3c4028872afa2c6a7fa2e1db62ffcae50bb
Henri Lahtinen <splitti@gmail.com>
2024-12-03 10:06:16 +0000

    day03: second part

--- day03.hs
+++ day03.hs
@@ -3,19 +3,23 @@
 module Main where
 
 import Control.Applicative ((*>), (<*), (<*>))
-import Control.Monad (msum)
+import Control.Monad (msum, void)
 import Data.Void (Void)
 import Text.Megaparsec
-import Text.Megaparsec.Char (char, eol, string)
+import Text.Megaparsec.Char (char, string)
 import Text.Megaparsec.Char.Lexer (decimal)
+import Data.Functor
 
 type Parser = Parsec Void String
 
-main = interact (unlines . sequence [part1])
+main = interact (unlines . sequence [part1, part2])
 
-part1 = ("Part 1: " ++) . show . sum . msum . parse parser ""
+part1 = ("Part 1: " ++) . show . sum . msum . parse (parser anySingle) ""
+part2 = ("Part 2: " ++) . show . sum . msum . parse (parser (skipDisabled <|> void anySingle)) ""
 
-parser = many (try $ manyTill anySingle (lookAhead (try mul)) *> mul <* optional eol)
+parser skip = many $ try (manyTill skip (lookAhead (try mul)) *> mul)
 
 mul :: Parser Int
 mul = string "mul" *> between (char '(') (char ')') ((*) <$> decimal <* char ',' <*> decimal)
+
+skipDisabled = try $ string "don't()" *> manyTill anySingle (void (string "do()") <|> eof) $> ()
