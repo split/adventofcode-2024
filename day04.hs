e7ab65e0c34cfbf6f0f2a9eea57e852434c9838b
Henri Lahtinen <splitti@gmail.com>
2024-12-05 16:49:54 +0200

    day04: change of direction, both solutions

--- day04.hs
+++ day04.hs
@@ -1,13 +1,35 @@
 module Main where
-import Control.Monad (ap)
-import Data.List (tails, transpose, elemIndices, isPrefixOf)
 
-main = interact (unlines . sequence [part1] . lines)
+import Control.Monad (ap, guard)
+import Data.Map qualified as M
+import Data.Maybe (isJust)
+import Data.Set qualified as S
 
-part1 :: [String] -> String
-part1 = ("Part 1: " ++) . show . count "XMAS" . ap (++) mirror . ap (++) diagonal . rotations
+type Point = (Int, Int)
 
-rotations = concat . take 4 . iterate transpose
-mirror = map reverse
-diagonal station = [drop i row | (row, i) <- zip station [0..]]
-count word = length . filter (isPrefixOf word) . tails . concat
+type Station = M.Map Point Char
+
+main = interact (unlines . sequence [part1, part2] . parse)
+
+part1, part2 :: Station -> String
+part1 = ("Part 1: " ++) . show . length . (\s -> concatMap (\o -> filter (\d -> has s d o "XMAS") dirs) (M.keys s))
+part2 = ("Part 2: " ++) . show . count . (\s -> concatMap (\o -> move o <$> filter (\d -> has s d o "MAS") diag) (M.keys s))
+
+count = M.size . M.filter (> 1) . M.fromListWith (+) . map (,1)
+
+has :: Station -> Point -> Point -> String -> Bool
+has station dir@(dx, dy) origin = go
+  where
+    go "" = True
+    go s@(c' : rest) = isJust $ do
+      c <- station M.!? move origin (dx * length rest, dy * length rest)
+      guard (c == c' && go rest)
+
+parse :: String -> M.Map Point Char
+parse input = M.fromList [((x, y), c) | (l, y) <- zip (lines input) [0 ..], (c, x) <- zip l [0 ..]]
+
+diag, dirs :: [(Int, Int)]
+diag = [(1, 1), (-1, 1), (-1, -1), (1, -1)]
+dirs = diag ++ [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+move (x, y) (dx, dy) = (x + dx, y + dy)
