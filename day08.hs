a0d909a7fe43393bb9fcb53b0f4be7ddd7c0d0c5
Henri Lahtinen <splitti@gmail.com>
2024-12-08 16:16:02 +0000

    day08: formating

--- day08.hs
+++ day08.hs
@@ -1,6 +1,7 @@
 module Main where
-import qualified Data.Set as S
-import qualified Data.Map.Strict as M
+
+import Data.Map.Strict qualified as M
+import Data.Set qualified as S
 
 main = interact (unlines . sequence [part1, part2] . scan)
 
@@ -8,9 +9,9 @@ part1 = ("Part 1: " ++) . show . length . antinodes [1]
 part2 = ("Part 2: " ++) . show . length . antinodes [0 .. 50]
 
 antinodes n = inMap (S.unions . S.map (uncurry offset) . pairs)
-    where
-        pairs = S.filter (uncurry (/=)) . (S.cartesianProduct <*> id)
-        inMap f = (S.intersection . S.unions) <*> (S.unions . M.map f . M.delete '.')
-        offset (x, y) (x', y') = S.fromList [(x + i*(x - x'), y + i*(y - y')) | i <- n]
+  where
+    pairs = S.filter (uncurry (/=)) . (S.cartesianProduct <*> id)
+    inMap f = (S.intersection . S.unions) <*> (S.unions . M.map f . M.delete '.')
+    offset (x, y) (x', y') = S.fromList [(x + i * (x - x'), y + i * (y - y')) | i <- n]
 
-scan input = M.fromListWith S.union [(c, S.singleton (x, y)) | (l, y) <- zip (lines input) [0..], (c, x) <- zip l [0..]]
+scan input = M.fromListWith S.union [(c, S.singleton (x, y)) | (l, y) <- zip (lines input) [0 ..], (c, x) <- zip l [0 ..]]
