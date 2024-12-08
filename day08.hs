a8a81917afdfa27818a1fed0f902939075f7871c
Henri Lahtinen <splitti@gmail.com>
2024-12-08 16:13:45 +0000

    day08: swap ap to <*>

--- day08.hs
+++ day08.hs
@@ -1,7 +1,6 @@
 module Main where
 import qualified Data.Set as S
 import qualified Data.Map.Strict as M
-import Control.Monad (ap)
 
 main = interact (unlines . sequence [part1, part2] . scan)
 
@@ -11,7 +10,7 @@ part2 = ("Part 2: " ++) . show . length . antinodes [0 .. 50]
 antinodes n = inMap (S.unions . S.map (uncurry offset) . pairs)
     where
         pairs = S.filter (uncurry (/=)) . (S.cartesianProduct <*> id)
-        inMap f = ap (S.intersection . S.unions) (S.unions . M.map f . M.delete '.')
+        inMap f = (S.intersection . S.unions) <*> (S.unions . M.map f . M.delete '.')
         offset (x, y) (x', y') = S.fromList [(x + i*(x - x'), y + i*(y - y')) | i <- n]
 
 scan input = M.fromListWith S.union [(c, S.singleton (x, y)) | (l, y) <- zip (lines input) [0..], (c, x) <- zip l [0..]]
