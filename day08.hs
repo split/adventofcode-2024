bed3f15dd57f95c61d1d783e63e587bce1661623
Henri Lahtinen <splitti@gmail.com>
2024-12-08 15:52:12 +0000

    day08: lazy solution to second part

--- day08.hs
+++ day08.hs
@@ -3,14 +3,15 @@ import qualified Data.Set as S
 import qualified Data.Map.Strict as M
 import Control.Monad (ap)
 
-main = interact (unlines . sequence [part1] . scan)
+main = interact (unlines . sequence [part1, part2] . scan)
 
-part1 = ("Part 1: " ++) . show . length . antinodes
+part1 = ("Part 1: " ++) . show . length . antinodes [1]
+part2 = ("Part 2: " ++) . show . length . antinodes [0 .. 50]
 
-antinodes = inMap (S.unions . S.map (uncurry offset) . pairs)
+antinodes n = inMap (S.unions . S.map (uncurry offset) . pairs)
     where
         pairs = S.filter (uncurry (/=)) . (S.cartesianProduct <*> id)
         inMap f = ap (S.intersection . S.unions) (S.unions . M.map f . M.delete '.')
-        offset (x, y) (x', y') = S.singleton (x + (x - x'), y + (y - y'))
+        offset (x, y) (x', y') = S.fromList [(x + i*(x - x'), y + i*(y - y')) | i <- n]
 
 scan input = M.fromListWith S.union [(c, S.singleton (x, y)) | (l, y) <- zip (lines input) [0..], (c, x) <- zip l [0..]]
