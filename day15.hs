c6c51db8d2d6dd0a0f05760f98ffbc1609c7739a
Henri Lahtinen <splitti@gmail.com>
2024-12-15 17:21:18 +0200

    day15: second part

--- day15.hs
+++ day15.hs
@@ -1,9 +1,10 @@
 module Main where
 
-import Control.Monad (guard)
+import Control.Monad (guard, when)
 import Data.List.Split (splitOn)
 import Data.Map qualified as M
 import Data.Maybe (fromMaybe)
+import Data.Set qualified as S
 
 type Pos = (Int, Int)
 
@@ -11,31 +12,54 @@ type Warehouse = M.Map Pos Char
 
 type Command = Char
 
-main = interact (unlines . sequence [part1 . parse])
+main = interact (unlines . sequence [part1 . parse, part2 . parse . scaleUp])
 
-part1 :: ((Warehouse, Pos), [Command]) -> [Char]
+part1, part2 :: ((Warehouse, Pos), [Command]) -> String
 part1 = ("Part 1: " ++) . show . sum . gps . uncurry simulate
+part2 = ("Part 2: " ++) . show . sum . gps . uncurry simulate
 
-gps = map (uncurry (+) . ((* 100) <$>)) . M.keys . M.filter (== 'O')
+gps = map (uncurry (+) . ((* 100) <$>)) . M.keys . M.filter (`elem` "O[")
 
 simulate :: (Warehouse, Pos) -> [Command] -> Warehouse
 simulate = (fst .) . foldl (\acc c -> fromMaybe acc (move acc c))
 
 move :: (Warehouse, Pos) -> Command -> Maybe (Warehouse, Pos)
-move (wh, pos) c = do
-  f <- wh M.!? pos
-  guard (f /= '.')
-  let pos' = newPos pos c
-  let wh' = maybe wh fst (move (wh, pos') c)
-  t <- wh' M.!? pos'
-  guard (t == '.')
-  return (M.insert pos' f $ M.insert pos '.' wh', pos')
+move = go
+  where
+    go (wh, pos) c = do
+      from <- wh M.!? pos
+      let pos' = newPos pos c
+      (wh', _) <- attempt (wh, pos') c
+      to <- wh' M.!? pos'
+      guard (to == '.')
+      return (M.insert pos' from $ M.insert pos '.' wh', pos')
 
+    attempt (wh, pos') c = do
+      to <- wh M.!? pos'
+      case to of
+        '.' -> return (wh, pos')
+        '[' | c `elem` "^v" -> do
+          (wh', _) <- go (wh, pos') c
+          go (wh', newPos pos' '>') c
+        ']' | c `elem` "^v" -> do
+          (wh', _) <- go (wh, pos') c
+          go (wh', newPos pos' '<') c
+        _ -> go (wh, pos') c
+
+newPos :: (Num a, Num b) => (a, b) -> Char -> (a, b)
 newPos (x, y) c = case c of
   '>' -> (x + 1, y)
   '<' -> (x - 1, y)
   '^' -> (x, y - 1)
   'v' -> (x, y + 1)
+
+scaleUp = concatMap go
+  where
+    go '#' = "##"
+    go 'O' = "[]"
+    go '.' = ".."
+    go '@' = "@."
+    go c = [c]
 
 parse :: String -> ((Warehouse, Pos), [Command])
 parse input = ((warehouse, start warehouse), concat $ lines commands)
@@ -43,6 +67,3 @@ parse input = ((warehouse, start warehouse), concat $ lines commands)
     [state, commands] = splitOn "\n\n" input
     warehouse = M.fromList [((x, y), c) | (y, row) <- zip [0 ..] (lines state), (x, c) <- zip [0 ..] row, c /= '#']
     start = (!! 0) . M.keys . M.filter (== '@')
-
-draw :: Warehouse -> String
-draw wh = unlines [[M.findWithDefault '#' (x, y) wh | x <- [0 .. 8]] | y <- [0 .. 8]] ++ "\n\n"
