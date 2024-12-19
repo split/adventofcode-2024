8af48691987bcd9867d37519bc113467230be0fa
Henri Lahtinen <splitti@gmail.com>
2024-12-19 12:34:22 +0200

    day19: inline buildDesigns

--- day19.hs
+++ day19.hs
@@ -3,14 +3,11 @@ module Main where
 import Data.List (isSuffixOf)
 import Data.List.Split (splitOn)
 
-main = interact (unlines . sequence [part1, part2] . uncurry buildDesigns . parse)
+main = interact (unlines . sequence [part1, part2] . uncurry (map . buildDesign) . parse)
 
 part1, part2 :: [Int] -> String
 part1 = ("Part 1: " ++) . show . length . filter (> 0)
 part2 = ("Part 2: " ++) . show . sum
-
-buildDesigns :: [String] -> [String] -> [Int]
-buildDesigns towels = map (buildDesign towels)
 
 buildDesign :: [String] -> String -> Int
 buildDesign towels xs = dp !! length xs
